aa4c827e5a001a0c4cc897e94e72f5d7deacfaac
Ahmet <ahmet@icon>
2025-02-10 11:39:00 +0500

    10.02.03

--- src/env.js
+++ src/env.js
@@ -1,3 +1,4 @@
 // const api = "https://192.168.1.9:5001/api";
-const api = "https://192.168.43.221:5001/api";
+// const api = "https://192.168.43.221:5001/api";
+const api = "https://192.168.1.49:5001/api";
 export { api };
