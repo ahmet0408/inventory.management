495b223c08176ba73bf0d43bebe27ee30ae0320b
Acer <Acer@DESKTOP-TH9L0R8>
2025-02-20 19:26:03 +0500

    20.02.2

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
-const api = "https://6970-118-240-118-77.ngrok-free.app/api";
+const api = "https://345a-119-235-118-190.ngrok-free.app/api";
 // const api = "http://localhost:5000/api";
 export { api };
