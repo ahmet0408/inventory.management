4f372f0f7f9af7a62f6f1d31d382646b3cec7cee
Acer <Acer@DESKTOP-TH9L0R8>
2025-02-24 00:02:51 +0500

    23.02.3

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
-// const api = "https://0de5-119-235-118-190.ngrok-free.app/api";
-const api = "http://localhost:5000/api";
+const api = "https://8bafe3d1a6063036e1640144b00d42c2.serveo.net/api";
+// const api = "http://localhost:5000/api";
 export { api };
